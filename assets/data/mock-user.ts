6501b9bdc015c8ab6118223a1cd2823761e2d7b7
maninesan8 <maninesan8@gmail.com>
2018-03-01 23:20:57 -0500

    Auto-generated commit

---
+++ assets/data/mock-user.ts
@@ -0,0 +1,71 @@
+export const MockUsers = {
+  users:
+    [
+      {
+        'firstName': 'admin',
+        'lastName': 'admin',
+        'email': 'adas@asd.com',
+        'password': '1234',
+        'confirmPassword': '1234',
+        'type': 'admin',
+        'company': null,
+        'username': 'aadmin',
+        'token': 's2s-token'
+      },
+      {
+        'firstName': 'customer',
+        'lastName': 'customer',
+        'email': 'adas@asd.com',
+        'password': '1234',
+        'confirmPassword': '1234',
+        'type': 'customer',
+        'company': 'Gibi Systems',
+        'username': 'ccustomer',
+        'token': 's2s-token'
+      },
+      {
+        'firstName': 'maninesan',
+        'lastName': 'ramanathan',
+        'email': 'mramanathan@test.com',
+        'password': '1234',
+        'confirmPassword': '1234',
+        'type': 'customer',
+        'company': 'Gibi Systems',
+        'username': 'mramanathan',
+        'token': 's2s-token'
+      },
+      {
+        'firstName': 'kumaran',
+        'lastName': 'ravi',
+        'email': 'kravi@asd.com',
+        'password': '1234',
+        'confirmPassword': '1234',
+        'type': 'customer',
+        'company': 'Gibi Systems',
+        'username': 'kravi',
+        'token': 's2s-token'
+      },
+      {
+        'firstName': 'deepak',
+        'lastName': 'customer',
+        'email': 'dcustomer@test.com',
+        'password': '1234',
+        'confirmPassword': '1234',
+        'type': 'customer',
+        'company': 'Gibi Systems',
+        'username': 'dcustomer',
+        'token': 's2s-token'
+      },
+      {
+        'firstName': 'prashant',
+        'lastName': 'srinivasan',
+        'email': 'psrinivasan@test.com',
+        'password': '1234',
+        'confirmPassword': '1234',
+        'type': 'customer',
+        'company': 'Gibi Systems',
+        'username': 'psrinivasan',
+        'token': 's2s-token'
+      }
+    ]
+};
